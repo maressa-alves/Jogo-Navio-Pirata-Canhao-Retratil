5f210b338253a18959363229d466b25018b6a161
Maressa Alves <87664261+maressa-alves@users.noreply.github.com>
2022-11-10 20:49:04 -0300

    Adicionamos canhão retrátil do Gui!

---
+++ js/Cannon.js
@@ -0,0 +1,35 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+  }
+  display() {
+    if (keyIsDown(RIGHT_ARROW) && this.angle < 0.35) {
+      this.angle += 0.02;
+    }
+
+    if (keyIsDown(LEFT_ARROW) && this.angle > -1.45) {
+      this.angle -= 0.02;
+    }
+console.log(cannon);
+    if(keyIsDown(DOWN_ARROW) && cannon.y <300 && this.angle <= -1.4) {
+      cannon.y = cannon.y +3
+    }
+    
+    if(keyIsDown(UP_ARROW) && cannon.y >110 && this.angle <= -1.4) {
+    cannon.y = cannon.y -3
+    }    
+
+    fill("#676e6a");
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    rect(-10, -20, this.width, this.height);
+    pop();
+    arc(this.x - 30, this.y + 90, 140, 200, PI, TWO_PI);
+    noFill();
+  }
+}
